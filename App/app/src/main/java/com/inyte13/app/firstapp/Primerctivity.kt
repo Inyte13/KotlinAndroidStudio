11d3b853a02d052e46b4a005db923cfff0c9c71b
Inyte13 <luisgardaz13@gmail.com>
2025-11-25 23:38:30 -0500

    app

---
+++ App/app/src/main/java/com/inyte13/app/firstapp/Primerctivity.kt
@@ -0,0 +1,21 @@
+package com.inyte13.app.firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.inyte13.app.R
+
+class Primerctivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_primerctivity)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
